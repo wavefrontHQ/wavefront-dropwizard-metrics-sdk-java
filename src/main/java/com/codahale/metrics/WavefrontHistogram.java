0316d0fb8f2d6ac2a135343ecc1ea8f5d5b39d71
Sushant Dewan <sushant@wavefront.com>
2018-08-23 13:10:13 -0700

    Support for Wavefront Dropwizard Metrics reporter (#2)
    * Support for reporter

---
+++ src/main/java/com/codahale/metrics/WavefrontHistogram.java
@@ -0,0 +1,172 @@
+package com.codahale.metrics;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import com.wavefront.sdk.entities.histograms.WavefrontHistogramImpl;
+
+import java.io.OutputStream;
+import java.util.List;
+import java.util.function.Supplier;
+
+/**
+ * WavefrontHistogram implementation for com.codahale.metrics
+ * Caveat: Cannot use the same WavefrontHistogram registry for multiple reporters as the reporter
+ * will change the state of the WavefrontHistogram every time the value is reported.
+ *
+ * @author Sushant Dewan (sushant@wavefront.com).
+ */
+public class WavefrontHistogram extends Histogram implements Metric {
+
+  private final WavefrontHistogramImpl delegate;
+
+  public static WavefrontHistogram get(MetricRegistry registry, String metricName) {
+    return get(registry, metricName, System::currentTimeMillis);
+  }
+
+  @VisibleForTesting
+  public static synchronized WavefrontHistogram get(MetricRegistry registry,
+                                                    String metricName,
+                                                    Supplier<Long> clock) {
+    // Awkward construction trying to fit in with Dropwizard Histogram
+    TDigestReservoir reservoir = new TDigestReservoir();
+    WavefrontHistogram tDigestHistogram = new WavefrontHistogram(reservoir, clock);
+    reservoir.set(tDigestHistogram);
+    try {
+      return registry.register(metricName, tDigestHistogram);
+    } catch(IllegalArgumentException e) {
+      Histogram existing = registry.histogram(metricName);
+      if (existing instanceof WavefrontHistogram) {
+        return (WavefrontHistogram) existing;
+      } else {
+        throw new IllegalStateException("Found non-WavefrontHistogram: " + existing);
+      }
+    }
+  }
+
+  private WavefrontHistogram(TDigestReservoir reservoir, Supplier<Long> clockMillis) {
+    super(reservoir);
+    delegate = new WavefrontHistogramImpl(clockMillis);
+  }
+
+  @Override
+  public void update(int value) {
+    delegate.update(value);
+  }
+
+  @Override
+  public void update(long value) {
+    delegate.update(value);
+  }
+
+  public void update(double value) {
+    delegate.update(value);
+  }
+
+  @Override
+  public long getCount() {
+    return delegate.getCount();
+  }
+
+  @Override
+  public Snapshot getSnapshot() {
+    final WavefrontHistogramImpl.Snapshot delegateSnapshot =
+        delegate.getSnapshot();
+
+    return new Snapshot() {
+      @Override
+      public double getMedian() {
+        return getValue(.50);
+      }
+
+      @Override
+      public double get75thPercentile() {
+        return getValue(.75);
+      }
+
+      @Override
+      public double get95thPercentile() {
+        return getValue(.95);
+      }
+
+      @Override
+      public double get98thPercentile() {
+        return getValue(.98);
+      }
+
+      @Override
+      public double get99thPercentile() {
+        return getValue(.99);
+      }
+
+      @Override
+      public double get999thPercentile() {
+        return getValue(.999);
+      }
+
+      @Override
+      public long getMax() {
+        return Math.round(delegateSnapshot.getMax());
+      }
+
+      @Override
+      public double getMean() {
+        return delegateSnapshot.getMean();
+      }
+
+      @Override
+      public long getMin() {
+        return (long) delegateSnapshot.getMin();
+      }
+
+      @Override
+      public double getStdDev() {
+        return delegate.stdDev();
+      }
+
+      @Override
+      public void dump(OutputStream outputStream) {
+      }
+
+      @Override
+      public double getValue(double quantile) {
+        return delegateSnapshot.getValue(quantile);
+      }
+
+      @Override
+      public long[] getValues() {
+        return new long[0];
+      }
+
+      @Override
+      public int size() {
+        return delegateSnapshot.getSize();
+      }
+    };
+  }
+
+  public List<WavefrontHistogramImpl.Distribution> flushDistributions() {
+    return delegate.flushDistributions();
+  }
+
+  private static class TDigestReservoir implements Reservoir {
+
+    private WavefrontHistogram wfHist;
+
+    void set(WavefrontHistogram tdm) {
+      this.wfHist = tdm;
+    }
+
+    @Override
+    public int size() { return (int) wfHist.getCount(); }
+
+    @Override
+    public void update(long l) {
+      wfHist.update(l);
+    }
+
+    @Override
+    public Snapshot getSnapshot() {
+      return wfHist.getSnapshot();
+    }
+  }
+}
---
+++ src/main/java/com/wavefront/dropwizard/metrics/Main.java
@@ -0,0 +1,132 @@
+package com.wavefront.dropwizard.metrics;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+import com.codahale.metrics.Clock;
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.DeltaCounter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricAttribute;
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import com.codahale.metrics.WavefrontHistogram;
+import com.wavefront.sdk.direct_ingestion.WavefrontDirectIngestionClient;
+import com.wavefront.sdk.proxy.WavefrontProxyClient;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Driver class for ad-hoc experiments
+ *
+ * @author Sushant Dewan (sushant@wavefront.com).
+ */
+public class Main {
+
+  public static void main(String[] args) throws InterruptedException, IOException {
+    String wavefrontServer = args[0];
+    String token = args[1];
+    String proxyHost = args.length < 3 ? null : args[2];
+    String metricsPort = args.length < 4 ? null : args[3];
+    String distributionPort = args.length < 5 ? null : args[4];
+
+    WavefrontProxyClient.Builder proxyBuilder = new WavefrontProxyClient.Builder(proxyHost);
+    if (metricsPort != null) {
+      proxyBuilder.metricsPort(Integer.parseInt(metricsPort));
+    }
+    if (distributionPort != null) {
+      proxyBuilder.distributionPort(Integer.parseInt(distributionPort));
+    }
+    WavefrontProxyClient wavefrontProxyClient = proxyBuilder.build();
+
+    WavefrontDirectIngestionClient wavefrontDirectIngestionClient =
+        new WavefrontDirectIngestionClient.Builder(wavefrontServer, token).build();
+
+    MetricRegistry metricRegistry = new MetricRegistry();
+    DropwizardMetricsReporter.Builder builder =
+        DropwizardMetricsReporter.forRegistry(metricRegistry);
+
+    /* Set the source for your metrics and histograms */
+    builder.withSource("mySource");
+
+    /* Invoke this method to report your metrics and histograms with given prefix */
+    builder.prefixedWith("myPrefix");
+
+    /* Explicitly set the clock */
+    builder.withClock(new Clock() {
+      @Override
+      public long getTick() {
+        return System.currentTimeMillis();
+      }
+    });
+
+    /* Set reporter level point tags map for your metrics and histograms */
+    builder.withReporterPointTags(ImmutableMap.<String, String>builder().
+        put("env", "Staging").
+        put("location", "SF").build());
+
+    /* Add a specific reporter level point tag key value for your metrics and histograms */
+    builder.withReporterPointTag("cluster", "us-west");
+
+    /* Only report metrics that begin with 'my*'  */
+    builder.filter(MetricFilter.startsWith("my"));
+
+    /* Don't report stddev and m15 */
+    builder.disabledMetricAttributes(ImmutableSet.<MetricAttribute>builder().
+        add(MetricAttribute.STDDEV).add(MetricAttribute.M15_RATE).build());
+
+    /* Invoke this method if you want to report JVM metrics for your Java app */
+    builder.withJvmMetrics();
+
+    /* Invoke this method if you want to report minute bin Wavefront histograms */
+    builder.reportMinuteDistribution();
+
+    /* Invoke this method if you want to report hour bin Wavefront histograms  */
+    builder.reportHourDistribution();
+
+    /* Invoke this method if you want to report day bin Wavefront histograms */
+    builder.reportDayDistribution();
+
+    DropwizardMetricsReporter dropwizardMetricsReporter =
+        builder.build(wavefrontDirectIngestionClient);
+
+    /*
+     * Instead of direct ingestion, you can also report the metrics and histograms to Wavefront via
+     * proxy using the below line of code
+     */
+    //DropwizardMetricsReporter dropwizardMetricsReporter = builder.build(wavefrontProxyClient);
+
+    /* Report metrics and histograms to Wavefront every 30 seconds */
+    dropwizardMetricsReporter.start(30, TimeUnit.SECONDS);
+
+    /* 'notMyCounter' won't be reported as it does not begin with prefix - 'my*'  */
+    Counter notReported = metricRegistry.counter("notMyCounter");
+    Counter counter = metricRegistry.counter("myCounter");
+    DeltaCounter deltaCounter = DeltaCounter.get(metricRegistry, "myDeltaCounter");
+    AtomicInteger bufferSize = new AtomicInteger();
+    Gauge gauge = metricRegistry.register("myGauge", () -> bufferSize.get());
+    Meter meter = metricRegistry.meter("myMeter");
+    Timer timer = metricRegistry.timer("myTimer");
+    Histogram dropwizardHistogram = metricRegistry.histogram("myDropwizardHistogram");
+    WavefrontHistogram wavefrontHistogram =
+        WavefrontHistogram.get(metricRegistry, "myWavefrontHistogram");
+
+    for (int i = 0; i < 50; i++) {
+      counter.inc();
+      deltaCounter.inc();
+      notReported.inc();
+      bufferSize.set(10 * i);
+      meter.mark(i);
+      timer.update(i, TimeUnit.SECONDS);
+      dropwizardHistogram.update(i);
+      wavefrontHistogram.update(i);
+      wavefrontHistogram.update(i * 1.0);
+      Thread.sleep(50);
+    }
+  }
+}
